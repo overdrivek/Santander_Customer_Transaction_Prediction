d91776d8f1fcf21361e7f280a4d93419932d62bd
Krishna Kumar Narayanan <k.narayanan@kostal.com>
2019-04-26 15:35:23 +0200

    brutal search forward chaining

--- src/Feature_selection_rfe.py
+++ src/Feature_selection_rfe.py
@@ -7,6 +7,18 @@ import numpy as np
 from sklearn.preprocessing import MinMaxScaler,StandardScaler
 from sklearn.neural_network import MLPClassifier
 from matplotlib import pyplot as plt
+from mlxtend.feature_selection import SequentialFeatureSelector as sfs
+from sklearn.metrics import accuracy_score as acc
+import mldashboard
+import tqdm
+
+log_dir = os.path.normpath('/home/naraya01/AEN/GIT/Santander/Santander_Customer_Transaction_Prediction/log/')
+if os.path.exists(log_dir) is False:
+    os.mkdir(log_dir)
+run_name = 'Santandar_RF_Forward_Chaining'
+mllogger = mldashboard.Logger()
+data_writer = mldashboard.JsonRecordFileWriter(log_dir,run_name,override=True)
+mllogger.add_writer(data_writer)
 
 train_file = os.path.normpath('/home/naraya01/AEN/GIT/Santander/Santander_Customer_Transaction_Prediction/Data/train.csv')
 test_file = os.path.normpath('/home/naraya01/AEN/GIT/Santander/Santander_Customer_Transaction_Prediction/Data/test.csv')
@@ -40,8 +52,8 @@ df_pristine_dropped = df_pristine_file.drop('ID_code', axis=1)
 #test_scaled = pd.DataFrame(scaler.transform(df_pristine_dropped),columns=df_pristine_dropped.columns)
 test_scaled = df_pristine_dropped
 #test_scaled = (df_pristine_dropped-df_input.min())/(df_input.max()-df_input.min())
-model_folder = os.path.normpath('/home/naraya01/AEN/GIT/Santander/Santander_Customer_Transaction_Prediction/Model/mrmr_iteration/MLP/')
-result_folder = os.path.normpath('/home/naraya01/AEN/GIT/Santander/Santander_Customer_Transaction_Prediction/Model/mrmr_iteration/Results/')
+model_folder = os.path.normpath('/home/naraya01/AEN/GIT/Santander/Santander_Customer_Transaction_Prediction/Model/forward_selected_rf')
+result_folder = os.path.normpath('/home/naraya01/AEN/GIT/Santander/Santander_Customer_Transaction_Prediction/Model/forward_selected_rf/Results/')
 if os.path.exists(model_folder) is False:
     os.mkdir(model_folder)
 
@@ -54,60 +66,67 @@ feature_list = []
 feature_num_list = []
 df_pristine_query = None
 from sklearn.feature_selection import RFECV
+train_errors = []
+test_errors = []
+max_features = 200
+features_selected = []
+total_num_features = x_train.shape[1]
+print('total number of features = ',x_train.shape[1])
+with tqdm.tqdm(total=max_features,desc='Feature selection ...') as timer:
+    for num_features in range(1,max_features+1,1):
 
-with open(log_file, 'a') as logger:
-    print('************************************************')
+        print('************************************************')
+        print('Selecting top {} features'.format(num_features))
+        # Run RF
+        classifier = RF_Classifier(n_estimators=total_num_features*num_features, verbose=2)
+        #clf_rf.fit(x_train[mrmr_output], y_train)
+        #classifier = svm.SVC(C=1.0,verbose=2)
+        #clf_rf.fit(x_train[mrmr_output], y_train)
 
-    # Run RF
-    #clf_rf = RF_Classifier(n_estimators=100, verbose=2)
-    #clf_rf.fit(x_train[mrmr_output], y_train)
-    classifier = svm.SVC(C=1.0,verbose=2)
-    #clf_rf.fit(x_train[mrmr_output], y_train)
+        #classifier = MLPClassifier(solver='adam', verbose=True, hidden_layer_sizes=(10,),momentum=0.0)
+        #selector = RFECV(classifier,step=1,cv=5)
+        selector = sfs(classifier,k_features=num_features,forward=True,floating=False,verbose=2,scoring='accuracy',cv=0)
+        selector = selector.fit(x_train,y_train)
 
-    #classifier = MLPClassifier(solver='adam', verbose=True, hidden_layer_sizes=(10,),momentum=0.0)
-    selector = RFECV(classifier,step=1,cv=5)
-    selector = selector.fit(x_train,y_train)
+        print("selected features ")
+        feature_cols = list(selector.k_feature_idx_)
+        features_selected.append(feature_cols)
 
-    print("selected features ")
-    print(selector.ranking_)
+        print("Training with the selected number of features")
+        classifier_rf = RF_Classifier(n_estimators=5000,verbose=2)
+        x_train_selected = x_train[:,feature_cols]
+        x_test_selected = x_test[:,feature_cols]
 
-    train_score = selector.score(x_train, y_train)
-    print('Train score = ',train_score)
-    train_accuracy.append(train_score)
+        print('Training ...')
+        classifier_rf.fit(x_test_selected,x_test_selected)
+        y_train_pred = classifier_rf.predict(x_train_selected)
 
-    test_score = classifier.score(x_test, y_test)
-    test_accuracy.append(test_score)
-    print('Validation score = ', test_score)
-    # feature_num_list.append(i)
-    # plt.clf()
-    # plt.ion()
-    # plt.show()
-    # plt.plot(np.asarray(feature_num_list), np.asarray(train_accuracy), 'r-', label='train accuracy')
-    # plt.plot(np.asarray(feature_num_list), np.asarray(test_accuracy), 'b-', label='test_accuracy')
-    # plt.grid(True)
-    # plt.xlabel('number of features')
-    # plt.ylabel('Accuracy')
-    # plt.legend()
-    # plt.draw()
-    # plt.pause(0.0001)
+        y_test_pred = classifier_rf.predict(x_test_selected)
 
-    print('Training done...')
-    print('Testing ...')
-    # pristine output
+        train_error = acc(y_train,y_train_pred)
+        test_error = acc(y_test,y_test_pred)
 
-    rf_predicted = classifier.predict(df_pristine_query)
+        print('Training done...')
+        print('Testing ...')
+        # pristine output
 
-    df_output = pd.concat([df_pristine_file['ID_code'], pd.DataFrame(rf_predicted, columns=['target'])], axis=1)
-    df_output.to_csv(os.path.join(result_folder,'MLP_'+str(i+1)+'_features.csv'),index=False)
-    print('done..')
-    pickle.dump(classifier, open(os.path.join(model_folder, 'rf_num_features_' + str(i + 1) + '.sav'), 'wb'))
+        rf_predicted = classifier_rf.predict(df_pristine_query)
 
-    #pickle.dump(scaler,open(os.path.join(model_folder,'scaler_num_features_'+str(i+1)+'.sav'),'wb'))
-    print('model saved. ')
+        df_output = pd.concat([df_pristine_file['ID_code'], pd.DataFrame(rf_predicted, columns=['target'])], axis=1)
+        df_output.to_csv(os.path.join(result_folder,'Test_RF_'+str(num_features)+'_features.csv'),index=False)
+        print('done..')
+        pickle.dump(classifier_rf, open(os.path.join(model_folder, 'rf_num_features_' + str(num_features) + '.sav'), 'wb'))
 
+        print('model saved. ')
 
+        mllogger.scalar('Train_errors',train_error,step=num_features)
+        mllogger.scalar('Test_errors', test_error, step=num_features)
+        mllogger.text('Selected features are '+str(feature_cols),step=num_features)
+        mllogger.text('Train error'+str(train_error),step=num_features)
+        mllogger.text('Test error' + str(test_error), step=num_features)
 
-plt.show()
+        timer.update()
+timer.close()
 
 
 
