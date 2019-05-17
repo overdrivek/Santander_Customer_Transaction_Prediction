8bf42c313250ef2f8dc3d433925bde23c7b709e9
Krishna Kumar Narayanan <k.narayanan@kostal.com>
2019-05-17 16:24:32 +0200

    light gbm added

--- src/playground_h2o.py
+++ src/playground_h2o.py
@@ -8,22 +8,20 @@ df_train_file = pd.read_csv(train_file)
 df_dropped_file = df_train_file.drop(['ID_code','target'],axis=1)
 
 column_names = list(df_dropped_file.columns.values)
-#df_grouped = df_dropped_file.groupby(column_names).size().reset_index(name='Count')
+
 
 # tests
 std= df_dropped_file['var_0'].std()
 mean = df_dropped_file['var_0'].mean()
 
 import  numpy as np
-#np_var = np.asarray(df_dropped_file['var_0'])
-#print(len(np.where(np.abs(np_var)< mean-1*std )[0]))
 
-from sklearn.preprocessing import StandardScaler,Normalizer
+from sklearn.preprocessing import StandardScaler,Normalizer, RobustScaler
 scaler = StandardScaler()
 df_dropped_file_scaled = scaler.fit_transform(np.asarray(df_dropped_file))
 
 # get training and validation files
-test_size = 0.05
+test_size = 0.01
 from sklearn.model_selection import train_test_split
 X_train,X_test,Y_train,Y_test = train_test_split(df_dropped_file_scaled,np.asarray(df_train_file['target']),test_size=test_size,shuffle=True,stratify=np.asarray(df_train_file['target']))
 
@@ -35,6 +33,33 @@ train_accuracies = []
 test_accuracies = []
 base_estimator = 'decTrees'
 
+from matplotlib import pyplot as plt
+from sklearn import metrics
+
+def modelfit(alg, dtrain,target, useTrainCV = True, cv_folds = 5, early_stopping_rounds = 50):
+    if useTrainCV:
+        xgb_param = alg.get_xgb_params()
+        xgtrain = xgb.DMatrix(dtrain, label=target)
+        cvresult = xgb.cv(xgb_param, xgtrain, num_boost_round=alg.get_params()['n_estimators'], nfold=cv_folds,
+                          metrics='auc', early_stopping_rounds=early_stopping_rounds)
+        alg.set_params(n_estimators=cvresult.shape[0])
+
+    # Fit the algorithm on the data
+    alg.fit(dtrain, target, eval_metric='auc')
+
+    # Predict training set:
+    dtrain_predictions = alg.predict(dtrain)
+    dtrain_predprob = alg.predict_proba(dtrain)[:, 1]
+
+    # Print model report:
+    print("\nModel Report")
+    print("Accuracy : %.4g" % metrics.accuracy_score(target, dtrain_predictions))
+    print("AUC Score (Train): %f" % metrics.roc_auc_score(target, dtrain_predprob))
+
+    feat_imp = pd.Series(alg.feature_importances_).sort_values(ascending=False)
+    #feat_imp.plot(kind='bar', title='Feature Importances')
+    #plt.ylabel('Feature Importance Score')
+
 
 #from h2o4gpu.ensemble import GradientBoostingClassifierSklearn as GradBoost
 from xgboost import XGBClassifier as xgb_classifier
@@ -43,11 +68,11 @@ import xgboost as xgb
 from h2o4gpu.linear_model import SGDClassifier as SGD
 feature_list = []
 feature_name_list = []
-
+# Set parameters
 num_round = 50
-maxdepth = 5
-tree_method = 'gpu_hist'
-grow_policy= 'lossguide'
+maxdepth = 3
+tree_method = 'gpu_exact'
+grow_policy= 'depthwise'
 max_depth= maxdepth
 random_state = 1234
 objective= 'binary:logistic'  # Specify multiclass classification
@@ -55,62 +80,80 @@ num_class = 2  # Number of possible output classes
 base_score = 0.5
 booster= 'gbtree'
 colsample_bylevel=1
-colsample_bytree=0.8
+colsample_bytree=0.9
 gamma=0
-learning_rate=0.1
+learning_rate=0.09
 max_delta_step=0
-min_child_weight=1
+min_child_weight=3
 missing=None
-n_estimators=1000
+n_estimators=10000
 scale_pos_weight=1
 silent=True
-subsample=0.8
+subsample=0.6
 verbose=True
 n_jobs=-1
 seed=27
 
-eval_metric = 'error'
-cv_folds = 20
+eval_metric = 'auc'
+cv_folds = 5
 early_stopping_rounds = 50
 
 gpu_res = {}  # Store accuracy result
-classifier_mm = xgb_classifier(learning_rate=learning_rate,tree_method=tree_method,grow_policy=grow_policy, objective=objective,eval_metric=eval_metric,booster=booster,n_estimators=n_estimators,max_depth=max_depth,
-                               min_child_weight=min_child_weight,gamma=gamma,colsample_bytree=colsample_bytree,seed=seed)
 
-xgb_param = classifier_mm.get_xgb_params()
+grid_search = False
 
-xgtrain = xgb.DMatrix(X_train, label=Y_train)
-cvresult = xgb.cv(xgb_param, xgtrain, num_boost_round=classifier_mm.get_params()['n_estimators'], nfold=cv_folds,metrics=eval_metric, early_stopping_rounds=early_stopping_rounds)
-classifier_mm.set_params(n_estimators=cvresult.shape[0])
+from sklearn.model_selection import GridSearchCV
+if grid_search is True:
+    param_test1 = {
+        #'max_depth': range(3, 10, 2),
+        #'min_child_weight': range(1, 6, 2),
+        #'gamma': [i / 10.0 for i in range(0, 5)]
+        #'subsample': [i / 10.0 for i in range(6, 10)],
+        #'colsample_bytree': [i / 10.0 for i in range(6, 10)]
+        'learning_rate':[0.15,0.1,0.09,0.08,0.07,0.06,0.05]
+    }
+    gsearch1 = GridSearchCV(estimator=xgb_classifier(learning_rate=learning_rate, n_estimators=n_estimators, max_depth=max_depth,
+                                                    min_child_weight=min_child_weight, gamma=gamma, subsample=subsample, colsample_bytree=colsample_bytree,
+                                                    objective='binary:logistic', scale_pos_weight=scale_pos_weight,seed=seed,tree_method=tree_method,nthread=4),
+                            param_grid=param_test1, scoring='roc_auc', n_jobs=4, iid=False, cv=5)
+    gsearch1.fit(X_train, Y_train)
+
+    print(gsearch1.cv_results_, gsearch1.best_params_, gsearch1.best_score_)
+else:
+
+    classifier_mm = xgb_classifier(learning_rate=learning_rate,tree_method=tree_method,grow_policy=grow_policy, objective=objective,eval_metric=eval_metric,booster=booster,n_estimators=n_estimators,max_depth=max_depth,
+                                   min_child_weight=min_child_weight,gamma=gamma,colsample_bytree=colsample_bytree,seed=seed,subsample=subsample,scale_pos_weight=scale_pos_weight)
+
+    modelfit(classifier_mm,X_train,Y_train)
+
+    #xgb_param = classifier_mm.get_xgb_params()
 
 
-classifier_mm.fit(X_train,Y_train.reshape(-1,1))
-
-train_output = classifier_mm.predict(X_train)
-train_accuracy = f1_score(Y_train,train_output)
-train_accuracies.append(train_accuracy)
-print('Train accuracy (F1) =',train_accuracy)
-from sklearn.metrics import confusion_matrix
-conf = confusion_matrix(Y_train,train_output)
-print('Train confusion matrix = \n',conf)
-
+    train_output = classifier_mm.predict(X_train)
+    train_f1_score = f1_score(Y_train, train_output)
+    print('Train F1 score =', train_f1_score)
+    train_accuracy = metrics.accuracy_score(Y_train,train_output)
+    print('Train accuracy = ',train_accuracy)
+    from sklearn.metrics import confusion_matrix
+    conf = confusion_matrix(Y_train,train_output)
+    print('Train confusion matrix = \n',conf)
 
 
-test_output = classifier_mm.predict(X_test)
-test_accuracy = f1_score(Y_test,test_output)
-test_accuracies.append(test_accuracy)
-print('Test accuracy (F1) =',test_accuracy)
-conf = confusion_matrix(Y_test,test_output)
-print('Test confusion matrix = \n',conf)
+    test_output = classifier_mm.predict(X_test)
+    test_f1_score = f1_score(Y_test, test_output)
+    print('Test accuracy (F1) =', test_f1_score)
+    test_accuracy = metrics.accuracy_score(Y_test,test_output)
+    print('Test accuracy = ',test_accuracy)
+    conf = confusion_matrix(Y_test,test_output)
+    print('Test confusion matrix = \n',conf)
 
-print('*************************************************')
-
- 
-# if plot_output is True:
-#     from matplotlib import pyplot as plt
-#     plt.plot(np.arange(num_features),train_accuracies,'r-')
-#     plt.plot(np.arange(num_features),test_accuracies,'b-')
-#     plt.grid(True)
-#     plt.xlabel('Features')
-#     plt.ylabel('Accuracies')
-#     plt.show()
+    print('*************************************************')
+    pristine_file = os.path.normpath('/home/naraya01/AEN/GIT/Santander/Santander_Customer_Transaction_Prediction/Data/test.csv')
+    df_pristine_file = pd.read_csv(pristine_file)
+    df_dropped_file = df_pristine_file.drop(['ID_code'],axis=1)
+    test_output = classifier_mm.predict(scaler.transform(np.asarray(df_dropped_file)))
+    df_output = pd.concat([df_pristine_file['ID_code'], pd.DataFrame(test_output, columns=['target'])], axis=1)
+    output_folder = os.path.normpath('/home/naraya01/AEN/GIT/Santander/Santander_Customer_Transaction_Prediction/Model/xgBoost/')
+    if os.path.exists(output_folder) is False:
+        os.mkdir(output_folder)
+    df_output.to_csv(os.path.join(output_folder,'pristine_result_'+grow_policy+'_'+tree_method+'_'+booster+'.csv'),index=False)
---
+++ src/playground_lgbm.py
@@ -0,0 +1,159 @@
+import pandas as pd
+import os
+from sklearn import metrics
+from matplotlib import pyplot as plt
+#from h2o4gpu.ensemble import GradientBoostingClassifierSklearn as GradBoost
+#from xgboost import XGBClassifier as xgb_classifier
+#import xgboost as xgb
+#from sklearn.linear_model import SGDClassifier as SGD
+#from h2o4gpu.linear_model import SGDClassifier as SGD
+import  numpy as np
+import lightgbm_gpu as lgbm
+from sklearn.preprocessing import StandardScaler,Normalizer, RobustScaler
+from sklearn.metrics import confusion_matrix
+
+
+train_file = os.path.normpath('/home/naraya01/AEN/GIT/Santander/Santander_Customer_Transaction_Prediction/Data/train.csv')
+df_train_file = pd.read_csv(train_file)
+df_dropped_file = df_train_file.drop(['ID_code','target'],axis=1)
+column_names = list(df_dropped_file.columns.values)
+
+# tests
+std= df_dropped_file['var_0'].std()
+mean = df_dropped_file['var_0'].mean()
+
+scaler = StandardScaler()
+df_dropped_file_scaled = scaler.fit_transform(np.asarray(df_dropped_file))
+
+# get training and validation files
+test_size = 0.1
+from sklearn.model_selection import train_test_split
+X_train,X_test,Y_train,Y_test = train_test_split(df_dropped_file_scaled,np.asarray(df_train_file['target']),test_size=test_size,shuffle=True,stratify=np.asarray(df_train_file['target']))
+
+train_data = lgbm.Dataset(X_train,label=Y_train)
+valid_data = train_data.create_valid(X_test,label=Y_test)
+print('Training and validation data generated.')
+
+
+plot_output = True
+from sklearn.metrics import f1_score
+train_accuracies = []
+test_accuracies = []
+base_estimator = 'decTrees'
+
+useTrainCV = False
+num_rounds = 10
+cv_folds = 10
+eval_metric = 'binary_error'
+num_leaves = 50
+num_trees = 1000
+objective = 'binary'
+learning_rate = 0.1
+boosting = 'rf'#'gbdt'
+num_iterations = 1000
+verbosity  = 1
+
+
+
+param = {'num_leaves': num_leaves,
+         'num_trees': num_trees,
+         'objective': objective,
+         'metric':eval_metric,
+         'learning_rate':learning_rate,
+         'boosting':boosting,
+         'num_iterations':num_iterations,
+         'verbosity':verbosity
+         }
+
+if useTrainCV:
+    cv_results = lgbm.cv(param, train_data, num_rounds, nfold=cv_folds)
+    lgbm_classifier = lgbm.train(param, train_data, num_rounds, valid_sets=[valid_data])
+else:
+    lgbm_classifier = lgbm.train(param,train_data,num_rounds,valid_sets=[valid_data])
+
+train_output = np.round(lgbm_classifier.predict(X_train))
+test_output = np.round(lgbm_classifier.predict(X_test))
+
+print('\n Train')
+print('Accuracy : %.4g'% metrics.accuracy_score(Y_train,train_output))
+train_f1_score = f1_score(Y_train, train_output)
+print('F1 score: %.4g' % train_f1_score)
+conf = confusion_matrix(Y_train,train_output)
+print('Train confusion matrix = \n',conf)
+
+print('\n Test')
+print('Accuracy : %.4g'% metrics.accuracy_score(Y_test,test_output))
+test_f1_score = f1_score(Y_test, test_output)
+print('F1 score: %.4g' % test_f1_score)
+conf = confusion_matrix(Y_test,test_output)
+print('Test confusion matrix = \n',conf)
+
+print('*************************************************')
+pristine_file = os.path.normpath('/home/naraya01/AEN/GIT/Santander/Santander_Customer_Transaction_Prediction/Data/test.csv')
+df_pristine_file = pd.read_csv(pristine_file)
+df_dropped_file = df_pristine_file.drop(['ID_code'],axis=1)
+test_output = lgbm_classifier.predict(scaler.transform(np.asarray(df_dropped_file)))
+df_output = pd.concat([df_pristine_file['ID_code'], pd.DataFrame(test_output, columns=['target'])], axis=1)
+output_folder = os.path.normpath('/home/naraya01/AEN/GIT/Santander/Santander_Customer_Transaction_Prediction/Model/lgbm/')
+if os.path.exists(output_folder) is False:
+    os.mkdir(output_folder)
+df_output.to_csv(os.path.join(output_folder,'pristine_result_.csv'),index=False)
+
+#
+#
+# grid_search = False
+#
+# from sklearn.model_selection import GridSearchCV
+# if grid_search is True:
+#     param_test1 = {
+#         #'max_depth': range(3, 10, 2),
+#         #'min_child_weight': range(1, 6, 2),
+#         #'gamma': [i / 10.0 for i in range(0, 5)]
+#         #'subsample': [i / 10.0 for i in range(6, 10)],
+#         #'colsample_bytree': [i / 10.0 for i in range(6, 10)]
+#         'learning_rate':[0.15,0.1,0.09,0.08,0.07,0.06,0.05]
+#     }
+#     gsearch1 = GridSearchCV(estimator=xgb_classifier(learning_rate=learning_rate, n_estimators=n_estimators, max_depth=max_depth,
+#                                                     min_child_weight=min_child_weight, gamma=gamma, subsample=subsample, colsample_bytree=colsample_bytree,
+#                                                     objective='binary:logistic', scale_pos_weight=scale_pos_weight,seed=seed,tree_method=tree_method,nthread=4),
+#                             param_grid=param_test1, scoring='roc_auc', n_jobs=4, iid=False, cv=5)
+#     gsearch1.fit(X_train, Y_train)
+#
+#     print(gsearch1.cv_results_, gsearch1.best_params_, gsearch1.best_score_)
+# else:
+#
+#     classifier_mm = xgb_classifier(learning_rate=learning_rate,tree_method=tree_method,grow_policy=grow_policy, objective=objective,eval_metric=eval_metric,booster=booster,n_estimators=n_estimators,max_depth=max_depth,
+#                                    min_child_weight=min_child_weight,gamma=gamma,colsample_bytree=colsample_bytree,seed=seed,subsample=subsample,scale_pos_weight=scale_pos_weight)
+#
+#     modelfit(classifier_mm,X_train,Y_train)
+#
+#     #xgb_param = classifier_mm.get_xgb_params()
+#
+#
+#     train_output = classifier_mm.predict(X_train)
+#     train_f1_score = f1_score(Y_train, train_output)
+#     print('Train F1 score =', train_f1_score)
+#     train_accuracy = metrics.accuracy_score(Y_train,train_output)
+#     print('Train accuracy = ',train_accuracy)
+
+
+
+
+    # test_output = classifier_mm.predict(X_test)
+    # test_f1_score = f1_score(Y_test, test_output)
+    # print('Test accuracy (F1) =', test_f1_score)
+    # test_accuracy = metrics.accuracy_score(Y_test,test_output)
+    # print('Test accuracy = ',test_accuracy)
+    # conf = confusion_matrix(Y_test,test_output)
+    # print('Test confusion matrix = \n',conf)
+
+    # print('*************************************************')
+    # pristine_file = os.path.normpath('/home/naraya01/AEN/GIT/Santander/Santander_Customer_Transaction_Prediction/Data/test.csv')
+    # df_pristine_file = pd.read_csv(pristine_file)
+    # df_dropped_file = df_pristine_file.drop(['ID_code'],axis=1)
+    # test_output = classifier_mm.predict(scaler.transform(np.asarray(df_dropped_file)))
+    # df_output = pd.concat([df_pristine_file['ID_code'], pd.DataFrame(test_output, columns=['target'])], axis=1)
+    # output_folder = os.path.normpath('/home/naraya01/AEN/GIT/Santander/Santander_Customer_Transaction_Prediction/Model/xgBoost/')
+    # if os.path.exists(output_folder) is False:
+    #     os.mkdir(output_folder)
+    # df_output.to_csv(os.path.join(output_folder,'pristine_result_'+grow_policy+'_'+tree_method+'_'+booster+'.csv'),index=False)
